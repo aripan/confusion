ac353f966ea77e8b66e2704a9d90337fe512b55d
aripan <aripan017@gmail.com>
2020-07-01 12:15:02 +0200

    Component Part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import "./App.css";
 import Menu from "./components/MenuComponent";
-import { dishes } from "./dishes";
+import "./App.css";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={dishes} />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,68 +1,65 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
-// import { dishes } from "../dishes";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
-// ! class component
-// class Menu extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: dishes,
-//     };
-//   }
+class Menu extends Component {
+  constructor(props) {
+    super(props);
 
-//   render() {
-//     const menu = this.state.dishes.map((dish) => {
-//       return (
-//         <div key={dish.id} className="col-12 mt-5">
-//           <Media tag="li">
-//             <Media left middle>
-//               <Media object src={dish.image} alt={dish.name} />
-//             </Media>
-//             <Media body className="ml-5">
-//               <Media heading>{dish.name}</Media>
-//               <p>{dish.description}</p>
-//             </Media>
-//           </Media>
-//         </div>
-//       );
-//     });
+    this.state = {
+      selectedDish: null,
+    };
+  }
 
-//     return (
-//       <div className="container">
-//         <div className="row">
-//           <Media list>{menu}</Media>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
 
-// ? writing the class component as function component
-const Menu = ({ dishes }) => {
-  const menu = dishes.map((dish) => {
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else return <div></div>;
+  }
+
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+
     return (
-      <div key={dish.id} className="col-12 mt-5">
-        <Media tag="li">
-          <Media left middle>
-            <Media object src={dish.image} alt={dish.name} />
-          </Media>
-          <Media body className="ml-5">
-            <Media heading>{dish.name}</Media>
-            <p>{dish.description}</p>
-          </Media>
-        </Media>
+      <div className="container">
+        <div className="row">{menu}</div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.state.selectedDish)}
+          </div>
+        </div>
       </div>
     );
-  });
-
-  return (
-    <div className="container">
-      <div className="row">
-        <Media list>{menu}</Media>
-      </div>
-    </div>
-  );
-};
+  }
+}
 
 export default Menu;
